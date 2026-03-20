81444fc98f5af167ce26249fdc955cb447c35d16
Kenan Ali <55302817+Kenan999@users.noreply.github.com>
2026-03-20 14:24:49 +0100

    Initial Commit

---
+++ Kali Note/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Kali Note
+//
+//  Created by Kenan Ali on 20.03.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
